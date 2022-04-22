0cc83c0ea563c32fd0dff0bdfe48eb16642ed9ea
Aleksandr Butakov <anbngm@gmail.com>
2022-04-22 10:53:28 +0500

    Added demowebshop tests

--- src/test/java/tests/BookstoreTests.java
+++ src/test/java/tests/BookstoreTests.java
@@ -2,20 +2,15 @@ package tests;
 
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
-import models.Credentials;
-import models.GenerateTokenResponse;
-import models.lombok.CredentialsLombok;
-import models.lombok.GenerateTokenResponseLombok;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
-import static io.restassured.http.ContentType.JSON;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static listeners.CustomAllureListener.withCustomTemplates;
-import static org.assertj.core.api.Assertions.assertThat;
+import static listener.CustomAllureListener.withCustomTemplates;
 import static org.hamcrest.Matchers.*;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class BookstoreTests {
 
@@ -24,207 +19,120 @@ public class BookstoreTests {
         RestAssured.baseURI = "https://demoqa.com";
     }
 
-
     @Test
     void getBooksTest() {
         get("/BookStore/v1/Books")
                 .then()
+                // проверим что вкладка "books" имеет размер больше чем 0
                 .body("books", hasSize(greaterThan(0)));
     }
 
+    // выводим все логи
     @Test
     void getBooksWithAllLogsTest() {
         given()
-                .log().all()
+                .log().all()    // логирование запроса
                 .when()
                 .get("/BookStore/v1/Books")
                 .then()
-                .log().all()
+                .log().all()    // логирование ответа
+                // проверим что вкладка "books" имеет размер больше чем 0
                 .body("books", hasSize(greaterThan(0)));
     }
 
+    // выводим логи выборочно
     @Test
     void getBooksWithSomeLogsTest() {
         given()
-                .log().uri()
+                .log().uri()    // логирование URI
+                .log().method()
                 .log().body()
                 .when()
                 .get("/BookStore/v1/Books")
                 .then()
-                .log().status()
+                .log().status()    // логирование ответа в консоли
                 .log().body()
+                // проверим что вкладка "books" имеет размер больше чем 0
                 .body("books", hasSize(greaterThan(0)));
     }
 
+    // проверяем получение токена с выборочным выводом логов
     @Test
     void generateTokenTest() {
         String data = "{ \"userName\": \"alex\", " +
                 "\"password\": \"asdsad#frew_DFS2\" }";
 
         given()
-                .contentType(JSON)
+                .contentType(ContentType.JSON)
                 .body(data)
-                .log().uri()
+                .log().uri()    // логирование URI
+                .log().method()
                 .log().body()
                 .when()
                 .post("/Account/v1/GenerateToken")
                 .then()
-                .log().status()
+                .log().status()    // логирование ответа
                 .log().body()
                 .statusCode(200)
+                // далее идут проверки
+                // проверим что вкладка "books" имеет размер больше чем 0
                 .body("status", is("Success"))
                 .body("result", is("User authorized successfully."))
-                .body("token.size()", (greaterThan(10)));
+                .body("token.size()", greaterThan(10));
     }
 
-    @Test
-    void getTokenTest() {
-        String data = "{ \"userName\": \"alex\", " +
-                "\"password\": \"asdsad#frew_DFS2\" }";
-
-        String token =
-                given()
-                        .contentType(JSON)
-                        .body(data)
-                        .log().uri()
-                        .log().body()
-                        .when()
-                        .post("/Account/v1/GenerateToken")
-                        .then()
-                        .log().status()
-                        .log().body()
-                        .statusCode(200)
-                        .body("status", is("Success"))
-                        .body("result", is("User authorized successfully."))
-                        .extract().path("token");
-
-        System.out.println("Token: " + token);
-    }
-
+    // добавляем allure listener
     @Test
     void generateTokenWithAllureListenerTest() {
         String data = "{ \"userName\": \"alex\", " +
                 "\"password\": \"asdsad#frew_DFS2\" }";
-
-//        RestAssured.filters(new AllureRestAssured()); move to @BeforeAll
-
+        // фильтр RestAssured можно перенести в @BeforeAll и он будет там вообще все перехватывать
+        // RestAssured.filters(new AllureRestAssured()); move to @BeforeAll логирование в Allure
         given()
-                .filter(new AllureRestAssured())
-                .contentType(JSON)
+                .filter(new AllureRestAssured())    // можем так индивидуально к запросу фильтр прописать
+                .contentType(ContentType.JSON)
                 .body(data)
-                .log().uri()
+                .log().uri()    // логирование URI
+                .log().method()
                 .log().body()
                 .when()
                 .post("/Account/v1/GenerateToken")
                 .then()
-                .log().status()
+                .log().status()    // логирование ответа
                 .log().body()
                 .statusCode(200)
+                // далее идут проверки
+                // проверим что вкладка "books" имеет размер больше чем 0
                 .body("status", is("Success"))
                 .body("result", is("User authorized successfully."))
-                .body("token.size()", (greaterThan(10)));
+                .body("token.size()", greaterThan(10));
     }
 
+    // добавляем allure listener
     @Test
     void generateTokenWithCustomAllureListenerTest() {
         String data = "{ \"userName\": \"alex\", " +
                 "\"password\": \"asdsad#frew_DFS2\" }";
-
+        // фильтр RestAssured можно перенести в @BeforeAll и он будет там вообще все перехватывать
+        // RestAssured.filters(new AllureRestAssured()); move to @BeforeAll логирование в Allure
         given()
-                .filter(withCustomTemplates())
-                .contentType(JSON)
+                .filter(withCustomTemplates())    // можем так индивидуально к запросу фильтр прописать
+                .contentType(ContentType.JSON)
                 .body(data)
-                .log().uri()
+                .log().uri()    // логирование URI
+                .log().method()
                 .log().body()
                 .when()
                 .post("/Account/v1/GenerateToken")
                 .then()
-                .log().status()
+                .log().status()    // логирование ответа
                 .log().body()
                 .statusCode(200)
+                // далее идут проверки
+                // проверим что вкладка "books" имеет размер больше чем 0
                 .body("status", is("Success"))
                 .body("result", is("User authorized successfully."))
-                .body("token.size()", (greaterThan(10)));
+                .body("token.size()", greaterThan(10));
     }
-
-    @Test
-    void generateTokenJsonSchemeCheckTest() {
-        String data = "{ \"userName\": \"alex\", " +
-                "\"password\": \"asdsad#frew_DFS2\" }";
-
-        given()
-                .filter(withCustomTemplates())
-                .contentType(JSON)
-                .body(data)
-                .log().uri()
-                .log().body()
-                .when()
-                .post("/Account/v1/GenerateToken")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("schemas/GenerateToken_response_scheme.json"))
-                .body("status", is("Success"))
-                .body("result", is("User authorized successfully."))
-                .body("token.size()", (greaterThan(10)));
-    }
-
-    @Test
-    void generateTokenWithModelTest() {
-        Credentials credentials = new Credentials();
-        credentials.setUserName("alex");
-        credentials.setPassword("asdsad#frew_DFS2");
-
-        GenerateTokenResponse tokenResponse =
-                given()
-                        .filter(withCustomTemplates())
-                        .contentType(JSON)
-                        .body(credentials)
-                        .log().uri()
-                        .log().body()
-                        .when()
-                        .post("/Account/v1/GenerateToken")
-                        .then()
-                        .log().status()
-                        .log().body()
-                        .statusCode(200)
-                        .body(matchesJsonSchemaInClasspath("schemas/GenerateToken_response_scheme.json"))
-                        .extract().as(GenerateTokenResponse.class);
-
-        assertThat(tokenResponse.getStatus()).isEqualTo("Success");
-        assertThat(tokenResponse.getResult()).isEqualTo("User authorized successfully.");
-        assertThat(tokenResponse.getExpires()).hasSizeGreaterThan(10);
-        assertThat(tokenResponse.getToken()).hasSizeGreaterThan(10).startsWith("eyJ");
-    }
-
-    @Test
-    void generateTokenWithLombokTest() {
-        CredentialsLombok credentials = new CredentialsLombok();
-        credentials.setUserName("alex");
-        credentials.setPassword("asdsad#frew_DFS2");
-
-        GenerateTokenResponseLombok tokenResponse =
-                given()
-                        .filter(withCustomTemplates())
-                        .contentType(JSON)
-                        .body(credentials)
-                        .log().uri()
-                        .log().body()
-                        .when()
-                        .post("/Account/v1/GenerateToken")
-                        .then()
-                        .log().status()
-                        .log().body()
-                        .statusCode(200)
-                        .body(matchesJsonSchemaInClasspath("schemas/GenerateToken_response_scheme.json"))
-                        .extract().as(GenerateTokenResponseLombok.class);
-
-        assertThat(tokenResponse.getStatus()).isEqualTo("Success");
-        assertThat(tokenResponse.getResult()).isEqualTo("User authorized successfully.");
-        assertThat(tokenResponse.getExpires()).hasSizeGreaterThan(10);
-        assertThat(tokenResponse.getToken()).hasSizeGreaterThan(10).startsWith("eyJ");
-    }
-
 
 }
